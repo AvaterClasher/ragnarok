53fe3f2d75b4e8f0975ace79423af91ba1d03f21
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2023-02-01 19:02:18 +0530

    new file:   .npmrc 	modified:   package-lock.json 	modified:   package.json 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/components/Footer/Footer.css 	new file:   src/components/Footer/Footer.jsx 	new file:   src/components/Sidebar/Sidebar.css 	new file:   src/components/Sidebar/Sidebar.jsx 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const sidebar = () => {
+  return (
+    <div>sidebar
+    {/*Twitter icon */}
+    
+    {/*Sidebar Option */}
+    {/*Sidebar Option */}
+    {/*Sidebar Option */}
+    {/*Sidebar Option */}
+    {/*Sidebar Option */}
+    {/*Sidebar Option */}
+
+    {/*Button -> Tweet*/}
+    </div>
+
+  )
+}
+
+export default sidebar
